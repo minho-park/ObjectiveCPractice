8a403fca1c84ba30ee8ea1982b050429c58018c8
Naver - Minho <Minho@Naverui-MacBook-Pro.local>
2015-06-18 16:21:40 +0900

    Initial Commit

---
+++ PracticeObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PracticeObjC
+//
+//  Created by Naver - Minho on 2015. 6. 18..
+//  Copyright (c) 2015년 Naver - Minho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
